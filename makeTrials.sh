3fc830f1cae37c304320982162230be89fac8b70
Jan Sindler <jansindl3r@gmail.com>
2021-01-14 10:20:46 +0100

    folders removed

--- makeTrials.sh
+++
@@ -1,6 +0,0 @@
-for font in full/*
-do 
-    out=${font/full/trial}
-    python trailer.py $font $out --keep-characters A B C D --keep-glyph-names space --keep-unicodes-base10 82 --replacer-character "*"
-    break
-done
